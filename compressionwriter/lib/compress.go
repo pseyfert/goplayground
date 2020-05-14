c9145923099ec30b46e2891e69d91991b241145b
Paul Seyfert <pseyfert.mathphys@gmail.com>
2020-05-14 21:38:16 +0200

    initial commit

---
+++ compressionwriter/lib/compress.go
@@ -0,0 +1,41 @@
+package lib
+
+import (
+	"compress/gzip"
+	"io"
+	"log"
+)
+
+type CompressReader struct {
+	underlying io.Reader
+	compressor *gzip.Writer
+	exposed    io.Reader
+}
+
+func NewCompressReader(r io.Reader) CompressReader {
+	retval := CompressReader{
+		underlying: r,
+	}
+	pr, pw := io.Pipe()
+	retval.compressor = gzip.NewWriter(pw)
+	retval.exposed = pr
+	go func() {
+		log.Print("launching copy")
+		for {
+			log.Printf("trying to copy")
+			n, err := io.Copy(retval.compressor, retval.underlying)
+			log.Printf("copied %d bytes", n)
+			if err == io.EOF || n == 0 {
+				break
+			}
+		}
+		log.Printf("closing: gzip.Writer.Close()")
+		retval.compressor.Close()
+		pw.Close()
+	}()
+	return retval
+}
+
+func (c *CompressReader) Read(b []byte) (int, error) {
+	return c.exposed.Read(b)
+}
